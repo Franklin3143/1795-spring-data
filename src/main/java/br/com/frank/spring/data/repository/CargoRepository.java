218cc482270abad5ef158f2459a7e33ff22a0d86
Franklin3143 <43715103+Franklin3143@users.noreply.github.com>
2020-11-25 09:57:31 -0300

    base do projeto finalizada

---
+++ src/main/java/br/com/frank/spring/data/repository/CargoRepository.java
@@ -0,0 +1,11 @@
+package br.com.frank.spring.data.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.frank.spring.data.orm.Cargo;
+
+@Repository
+public interface CargoRepository extends CrudRepository<Cargo, Integer> {
+
+}
---
+++ src/test/java/br/com/frank/spring/data/SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frank.spring.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
